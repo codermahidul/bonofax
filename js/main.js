3c1c67f3f6f1592862451debc188d6deeb248c24
codermahidul <codermahidul@gmail.com>
2023-09-08 23:21:22 +0600

    Main Menu Added wiht some error

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(function(){
+    'use strict';
+    
+    
+// $('.search_form').hide();
+
+$(".topMenuSearch").click(function(){
+    $('.search_form').addClass('show');
+  });
+
+$('.closeSearchForm').click(function(){
+    $('.search_form').removeClass('show');
+});
+
+
+   //=====NICE SELECT=======
+   $('#nice-select-one').niceSelect();
+   $('#nice-select-two').niceSelect();
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
