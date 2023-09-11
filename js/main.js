5b734ebfe66e9cfd485d927f1021c0f916adad3c
codermahidul <codermahidul@gmail.com>
2023-09-11 19:51:48 +0600

    Service Section Add

--- js/main.js
+++ js/main.js
@@ -30,6 +30,43 @@ $('.closeSearchForm').click(function(){
     autoPlay:false,
     slidesToShow: 1,
   });
+
+  $('.serviceSlider').slick({
+    dots: true,
+    infinite: false,
+    speed: 400,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+      
+
+
     
     
     
